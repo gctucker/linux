4b21f2c536ae1466a3cbb3646e68a09834a0a830
Guillaume Tucker <guillaume.tucker@collabora.com>
2017-06-21 09:25:27 +0100

    NOISE Flat is better than nested

--- init/initramfs.c
+++ init/initramfs.c
@@ -617,6 +617,7 @@ static int __init populate_rootfs(void)
 	printk(KERN_EMERG "*** Random lines from the Zen of Python ***\n");
 	printk(KERN_EMERG "Simple is better than complex.\n");
 	printk(KERN_EMERG "Complex is better than complicated.\n");
+	printk(KERN_EMERG "Flat is better than nested.\n");
 #endif
 	/* If available load the bootloader supplied initrd */
 	if (initrd_start && !IS_ENABLED(CONFIG_INITRAMFS_FORCE)) {
