4c813cad5efded0de883a1a041dbca0927930d46
Guillaume Tucker <guillaume.tucker@collabora.com>
2018-02-22 12:32:53 +0000

    Revert "NOISE Special cases aren't special enough..."
    This reverts commit 32aa0ca26063ebed7867cdbdb188a2bb27aad032.

--- init/initramfs.c
+++ init/initramfs.c
@@ -620,7 +620,6 @@ static int __init populate_rootfs(void)
 	printk(KERN_EMERG "Flat is better than nested.\n");
 	printk(KERN_EMERG "Sparse is better than dense.\n");
 	printk(KERN_EMERG "Readability counts.\n");
-	printk(KERN_EMERG "Special cases aren't special enough to break the rules.\n");
 #endif
 	/* If available load the bootloader supplied initrd */
 	if (initrd_start && !IS_ENABLED(CONFIG_INITRAMFS_FORCE)) {
